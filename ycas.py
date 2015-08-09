da2431b8a3062dac816666efc45b8358beb551bd
felgari <felgari@gmail.com>
2015-08-09 19:05:50 +0200

    Removed a print used for debug.

--- ycas.py
+++ ycas.py
@@ -56,8 +56,6 @@ def get_pipeline_parameters(progargs):
     if progargs.file_of_stars_provided:        
         # Read the data of the stars of interest.
         stars = starsset.StarsSet(progargs.stars_file_name)  
-        
-        print stars.star_names  
     
     filters = orgfits.Filters(progargs.filters_file_name)
     
