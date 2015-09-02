ecdc1b3b222e6bb5fc9eb7f602962a5918ff917e
felgari <felgari@gmail.com>
2015-09-02 23:06:22 +0200

    Removed the code to generate the code as an independent program.

--- curves.py
+++ curves.py
@@ -30,66 +30,17 @@ import argparse
 import magnitude
 from constants import *
 
-CSV_FILE_DELIM = ','
+TSV_FILE_DELIM = '\t'
 
 MJD_COL = 3
 MAG_COL = 4
 ERR_COL = 5
 
 NO_VALUE = "NA"
-
-class CurvesArguments(object):
-    """Encapsulates the definition and processing of program arguments."""
-    
-    MIN_NUM_ARGVS = 1  
-    
-    def __init__(self):
-        """Initializes parser. 
-        
-        Initialization of variables and the object ImagesArguments 
-        with the definition of arguments to use.
-
-        """         
-            
-        # Initiate arguments of the parser.
-        self.__parser = argparse.ArgumentParser()
-        
-        self.__parser.add_argument("-s", metavar="stars_file_name",
-                                   dest="s",
-                                   help="File of the stars to analyze.")  
-           
-    @property    
-    def file_of_stars_provided(self): 
-        return self.__args.s is not None      
-    
-    @property
-    def stars_file_name(self):
-        return self.__stars_file_name 
-    
-    def parse(self):
-        """Initialize properties and performs the parsing of program arguments.
-        
-        """
-        
-        # Parse program arguments.
-        self.__args = self.__parser.parse_args()
-            
-        if self.file_of_stars_provided:
-            self.__stars_file_name = self.__args.s    
-            
-    def print_usage(self):
-        """Print arguments options """
-        
-        self.__parser.print_usage()    
-        
-    def print_help(self):
-        """Print help for arguments options """
-                
-        self.__parser.print_help() 
         
 class LightCurves(object):
     """Class to generate light curves from the magnitudes calculated for the
-    objects of interes.
+    objects of interest.
     
     Two types of light curves are generated. One type of curves shows 
     differential magnitudes and the other one shows magnitudes calibrated.
@@ -173,53 +124,3 @@ def generate_curves(stars, stars_mag):
     if stars_mag is None:
         # TODO
         pass
-
-def main(progargs):
-    """Main function.
-    
-    Args:
-        progargs: Program arguments.
-        
-    Returns:    
-        Exit value.
-    
-    """
-    
-    exit_value = 0
-    
-    # Set the file, format and level of logging output.
-    logging.basicConfig(filename=DEFAULT_LOG_FILE_NAME, \
-                        format="%(asctime)s:%(levelname)s:%(message)s", \
-                        level=logging.DEBUG)   
-    
-    # Check if enough arguments have been received.
-    if len(sys.argv) < CurvesArguments.MIN_NUM_ARGVS:
-        print 
-        
-        exit_value = 1
-    else:        
-        files_data = read_input_files(progargs.input_files_names, \
-                                      progargs.calculate_median)
-            
-    return exit_value
-
-# Where all begins ...
-if __name__ == "__main__":
-    
-    # Create object to process program arguments.
-    progargs = CurvesArguments()    
-    
-    # Process program arguments.
-    progargs.parse()         
-    
-    # If no enough arguments are provided, show help and exit.
-    if len(sys.argv) <= CurvesArguments.MIN_NUM_ARGVS:
-        
-        print "At least %d arguments may be provided." % \
-            (CurvesArguments.MIN_NUM_ARGVS)
-                    
-        progargs.print_help()
-        
-        sys.exit(1)
-    else: 
-        sys.exit(main(progargs))
